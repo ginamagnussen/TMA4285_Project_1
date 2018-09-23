2a3af5715c505190859efbdbb495d9df139292de
ginamagnussen <31470965+ginamagnussen@users.noreply.github.com>
2018-09-23 22:16:48 +0200

    Small fixes

--- Ex3_TimeSeries.R
+++ Ex3_TimeSeries.R
@@ -9,7 +9,6 @@ library("ggplot2")
 
 
 # Import ARMA-series
-#setwd("~/Tidsrekker/TMA4285_Project_1")
 dataseries <- ts(read.table("DataEx3G14.txt"))
 
 
@@ -33,17 +32,18 @@ adf.test(dataseries) # Augmented Dickey-Fuller test
 ## ARIMA
 arimaFit <- arima(dataseries, order = c(2,0,1), method = "ML")
 arimaFit
-arimaFit$sigma2
 res_scaled <- arimaFit$residuals/sqrt(arimaFit$sigma2)
-plot(res_scaled)
-hist(res_scaled)
+plot(res_scaled, main ="Rescaled residuals")
+hist(res_scaled, main ="Histogram of rescaled residuals")
 
 ## Order selection
-acf(res_scaled) # To find order of MA(q) (lags)
-pacf(res_scaled) # To find order of AR(p)
 
-acf(dataseries)
-pacf(dataseries)
+# To check if chosen model is good
+acf(res_scaled) 
+pacf(res_scaled) 
+
+acf(dataseries) # To find order of MA(q) (lags)
+pacf(dataseries) # To find order of AR(p)
 
 vcov(arimaFit)
 
@@ -57,16 +57,16 @@ phi1 <- arimaFit$coef[1]; phi2 <- arimaFit$coef[2]; theta1 <- arimaFit$coef[3]; 
 for (t in 3:500){
   Xhat[t] = phi1*dataseries[t-1] + phi2*dataseries[t-2] + theta1*res[t-1] + res[t]
 }
-plot(401:500, dataseries[401:500], "l")
+plot(401:500, dataseries[401:500], "l", main = "Time series and predicted values", xlab = "Time step", ylab = "Value")
 lines(401:500, Xhat[401:500], col = "red")
-# Add title, xlab and ylab
+
 
 
 # Forecast
 ##################################################################
 fcast = forecast(arimaFit, h=20)
 pred <- predict(arimaFit, n.ahead = 20, se.fit = TRUE)
-plot(fcast)
+pplot(fcast)
 
 
 
@@ -83,7 +83,7 @@ for(p in 1:5){
   }
 }
 frame <- frame[-1,]
-ggplot(frame, aes(p,q)) + geom_raster(aes(fill = aic)) #+ scale_fill_gradientn(colours=c("#0000FFFF","#FFFFFFFF","#FF0000FF")) # Alternative color gradient
+ggplot(frame, aes(p,q)) + geom_raster(aes(fill = aic))
 
 
 
